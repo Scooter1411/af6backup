ba703023ae32c7a913378a0b889bc233fff4a1fe
alexander.franz.1411@gmail.com <alexander.franz.1411@gmail.com@884ed0b3-71b4-c2c8-87cb-37b2ad9281ac>
2014-05-23 16:18:25 +0000

    geht

--- trunk/af6backup.sh
+++ trunk/af6backup.sh
@@ -31,299 +31,6 @@ NAMESDIR=$TODIR/af6backup.names
 #############################################################
 MAILTO="alexander.franz.1411@gmail.com"
 #############################################################
-# serverBackup, awk-Teil
-#############################################################
-cat <<EOF > $TMP.serverBackup.awk
-BEGIN{
-    if( ENVIRON["AF5_DEBUG"] == "true" ){
-        print "Debug mode."
-        debug=1
-    }
-    af6readlist(inlist);    
-}
-{
-    ## legacy read from stdin
-
-    ## legacy file format:
-    ## <md5>;<size>;<crc>;<source path>
-
-    ## af6 file format:
-    ## <md5>;<size>;<mdate>;<host>;<source path>
-
-    l_listcount = split( \$0, l_list, ";" );
-    if( l_listcount == 6 ){
-        # we allow for one semicolon in file name (but remove it for the list)
-        l_list[5] = l_list[5] "_" l_list[6];
-        l_listcount = 5;
-    }
-    if(      l_listcount != 5            ) readerr("wrong.listcount",l_line)
-    else if( length(l_list[1]) != 32     ) readerr("wrong.length.of.md5.sum",l_line)
-    else if( l_list[1] !~ "^[0-9a-f]*\$" ) readerr("md5.not.hexadecimal",l_line)
-    else if( length(l_list[2]) < 1       ) readerr("size.empty",l_line)
-    else if( l_list[2] !~ "^[0-9]*\$"    ) readerr("size.not.a.number",l_line)
-    else if( length(l_list[3]) < 1       ) readerr("mdate.empty",l_line)
-    else if( l_list[3] !~ "^[0-9]*\$"    ) readerr("mdate.empty",l_line)
-    else if( length(l_list[4]) < 1       ) readerr("host.empty",l_line)
-    else if( length(l_list[5]) < 1       ) readerr("file.name.empty",l_line)
-    else {
-        # all is well
-        map_srcname_md5[l_list[5]";"l_list[1]] = 1;
-        if( length( map_md5_size[l_list[1]] ) == 0 ){
-            l_diffnum++;
-            map_md5_size[l_list[1]]       = l_list[2];
-            map_md5_mdate[l_list[1]]      = l_list[3];
-            map_md5_host[l_list[1]]       = l_list[4];
-            res = "DOBACKUP"
-        } else {
-            if( map_md5_size[l_list[1]] != l_list[2] ) readerr("size.collision",l_line);
-            res = "ALREADYDONE"
-        }
-        map_md5_srcname[l_list[1]]=l_list[5];
-        l_num++;
-    }
-}
-END{
-    af6writelist(outlist);    
-    print res
-}
-EOF
-#############################################################
-# legacyCombine, awk-Teil
-#############################################################
-cat <<EOF > $TMP.legacyCombine.awk
-BEGIN{
-    if( ENVIRON["AF5_DEBUG"] == "true" ){
-        print "Debug mode."
-        debug=1
-    }
-    af6readlist(inlist);    
-}
-{
-    ## legacy read from stdin
-
-    ## legacy file format:
-    ## <md5>;<size>;<crc>;<source path>
-
-    ## af6 file format:
-    ## <md5>;<size>;<mdate>;<host>;<source path>
-
-    if( substr(\$1,1,3) == pattern ){
-        l_listcount = split( \$0, l_list, ";" );
-        if( l_listcount == 5 ){
-            # we allow for one semicolon in file name (but remove it for the list)
-            l_list[4] = l_list[4] "_" l_list[5];
-            l_listcount = 4;
-        }
-        if(      l_listcount != 4            ) readerr("wrong.listcount",l_line)
-        else if( length(l_list[1]) != 32     ) readerr("wrong.length.of.md5.sum",l_line)
-        else if( l_list[1] !~ "^[0-9a-f]*\$" ) readerr("md5.not.hexadecimal",l_line)
-        else if( length(l_list[2]) < 1       ) readerr("size.empty",l_line)
-        else if( l_list[2] !~ "^[0-9]*\$"    ) readerr("size.not.a.number",l_line)
-        else if( length(l_list[4]) < 1       ) readerr("file.name.empty",l_line)
-        else {
-            # all is well
-            map_srcname_md5[l_list[4]";"l_list[1]] = 1;
-            if( length( map_md5_size[l_list[1]] ) == 0 ){
-                l_diffnum++;
-                map_md5_size[l_list[1]]       = l_list[2];
-                map_md5_mdate[l_list[1]]      = filedate( l_list[1] )
-            } else {
-                if( map_md5_size[l_list[1]] != l_list[2] ) readerr("size.collision",l_line);
-            }
-            map_md5_srcname[l_list[1]]=l_list[4];
-            map_md5_host[l_list[1]]="elefant";
-            l_num++;
-        }
-        
-    }
-}
-END{
-    af6writelist(outlist);    
-}
-EOF
-#############################################################
-# awk-Bibliothek
-#############################################################
-cat <<EOF > $TMP.lib.awk
-function tgtname( md5 ){
-    return "$TODIR" "/" substr(md5,1,1) "/" substr(md5,2,1) "/" substr(md5,3,1) "/" md5;
-}
-function filedate( name, l_cmd, l_line, l_word, l_res ){
-    l_cmd = "ls -l --time-style=\"+%Y%m%d%H%M%S\" \\"" tgtname( name ) "\\" 2>/dev/null";
-    l_cmd | getline l_line;
-    close(l_cmd);
-    split( l_line, l_word, " " );
-    if( length( l_word[6] ) > 0 ) {
-        l_res = l_word[6];
-    } else {
-        l_cmd = "ls -l --time-style=\"+%Y%m%d%H%M%S\" \\"" tgtname( name ) ".bz2\\" 2>/dev/null";
-        l_cmd | getline l_line;
-        close(l_cmd);
-        split( l_line, l_word, " " );
-        if( length( l_word[6] ) > 0 ) {
-            l_res = l_word[6];
-        } else {
-            l_res = 20131111111111;
-        }
-    }
-    return l_res;
-}
-function readerr( message, line, l_file ){
-    print message line
-    l_file = "$KAPUTT" "." message;
-    print line >> l_file;
-    errnum++;
-}
-##
-## af6 file format:
-## <md5>;<size>;<mdate>;<host>;<source path>
-##
-## parameters: filename (the file name)
-## local variables: l_eof, l_line, l_list, l_num, l_diffnum, l_listcount, l_msg, l_tnull, l_teins
-## global variables: errnum          (# errors encountered)
-##                   map_srcname_md5 (map original file name -> md5)
-##                   map_md5_size    (map md5 -> original size)
-##                   map_md5_mdate   (map md5 -> mdate)
-##                   map_md5_srcname (map md5 -> original file name)
-##
-function af6readlist( filename, l_eof, l_line, l_list, l_num, l_diffnum, l_listcount, l_msg, l_tnull, l_teins  ){
-
-    l_tnull = systime();
-    l_num = 0;
-    l_diffnum = 0;
-    errnum = 0;
-    l_eof = getline l_line < filename;
-    while( l_eof == 1 ){
-        l_listcount = split( l_line, l_list, ";" );
-        if( l_listcount == 6 ){
-            # we allow for one semicolon in file name (but remove it for the list)
-            l_list[4] = l_list[5] "_" l_list[6];
-            l_listcount = 5;
-        }
-        if(      l_listcount != 5            ) readerr("wrong.listcount",l_line)
-        else if( length(l_list[1]) != 32     ) readerr("wrong.length.of.md5.sum",l_line)
-        else if( l_list[1] !~ "^[0-9a-f]*\$" ) readerr("md5.not.hexadecimal",l_line)
-        else if( length(l_list[2]) < 1       ) readerr("size.empty",l_line)
-        else if( l_list[2] !~ "^[0-9]*\$"    ) readerr("size.not.a.number",l_line)
-        else if( length(l_list[3]) < 1       ) readerr("mdate.empty",l_line)
-        else if( l_list[3] !~ "^[0-9]*\$"    ) readerr("mdate.empty",l_line)
-        else if( length(l_list[4]) < 1       ) readerr("host.empty",l_line)
-        else if( length(l_list[5]) < 1       ) readerr("file.name.empty",l_line)
-        else {
-            # all is well
-            map_srcname_md5[l_list[5]";"l_list[1]] = 1;
-            if( length( map_md5_size[l_list[1]] ) == 0 ){
-                l_diffnum++;
-                map_md5_size[l_list[1]]       = l_list[2];
-                map_md5_mdate[l_list[1]]      = l_list[3];
-                map_md5_host[l_list[1]]       = l_list[4];
-            } else {
-                if( map_md5_size[l_list[1]] != l_list[2] ) readerr("size.collision",l_line);
-            }
-            map_md5_srcname[l_list[1]]=l_list[5];
-            l_num++;
-        }
-        l_eof = getline l_line  < filename;
-    }
-    l_teins = systime();
-    printf("Read within %d sec %7d lines/%6d files of %s.\n",l_teins - l_tnull,l_num,l_diffnum,filename);
-    if( debug || errnum > 0 ){
-        printf("Found %s errors.\n",errnum);
-    }
-    close(filename);   
-}
-##
-## af6 file format:
-## <md5>;<size>;<mdate>;<host>;<source path>
-##
-## parameters: filename (the file name)
-## local variables: l_eof, l_line, l_list, l_num, l_diffnum, l_listcount, l_msg, l_tnull, l_teins
-## global variables: errnum          (# errors encountered)
-##                   map_srcname_md5 (map original file name -> md5)
-##                   map_md5_size    (map md5 -> original size)
-##                   map_md5_mdate   (map md5 -> mdate)
-##                   map_md5_srcname (map md5 -> original file name)
-##
-function af6writelist( filename, l_key, l_list, l_md5, l_srcname, l_num, l_tnull, l_teins, l_tzwei, l_tdrei ){
-
-    l_num  = 0;
-    l_diff = 0;
-    l_tnull = systime();
-    l_cmd = sprintf("cat /dev/null > %s",filename)
-    system(l_cmd);
-    l_teins = systime();
-    for( l_key in map_srcname_md5) {
-         split( l_key, l_list, ";" );
-         l_srcname = l_list[1];
-         l_md5     = l_list[2];
-         if( length( map_md5_size[l_md5] ) > 0 ){
-             printf("%s;%s;%s;%s;%s\n",l_md5,map_md5_size[l_md5],map_md5_mdate[l_md5],map_md5_host[l_md5],l_srcname) >> filename;
-             l_num++;
-         }
-    }
-    close(filename);   
-    l_tzwei = systime();
-    printf("Wrote %7d lines to %s.\n",l_num,filename);
-    if( debug ) {
-        printf("   time to clean temporary file: %d sec.\n", l_teins - l_tnull );
-        printf("   time to write temporary file: %d sec.\n", l_tzwei - l_teins );
-        printf("   time to move to final file:   %d sec.\n", l_tdrei - l_tzwei );
-        printf("   total time:                   %d sec.\n", l_tdrei - l_tnull );
-    }    
-}
-EOF
-cat $TMP.lib.awk >> $TMP.serverBackup.awk
-cat $TMP.lib.awk >> $TMP.legacyCombine.awk
-############################################################
-# start mutex section
-############################################################
-af6_mutex_in () {
-    PID=/var/run/$BASE.pid
-    if [ -s $PID ] ; then
-        ps -e|grep `cat $PID` > /dev/null
-        if [ "$?" == "0" ] ; then
-            echo other process `cat $PID` still running|logger -s -puser.err -t$BASE.$$ 
-            sleep 10
-            if [ -s $PID ] ; then
-                ps -e|grep `cat $PID` > /dev/null
-                if [ "$?" == "0" ] ; then
-                    echo other process `cat $PID` still running|logger -s -puser.err -t$BASE.$$ 
-                    sleep 100
-                    if [ -s $PID ] ; then
-                        ps -e|grep `cat $PID` > /dev/null
-                        if [ "$?" == "0" ] ; then
-                            echo other process `cat $PID` still running|logger -s -puser.err -t$BASE.$$ 
-                            sleep 1000
-                            if [ -s $PID ] ; then
-                                ps -e|grep `cat $PID` > /dev/null
-                                if [ "$?" == "0" ] ; then
-                                    echo other process `cat $PID` still running|logger -s -puser.err -t$BASE.$$ 
-                                    exit 42
-                                else
-                                    echo other process `cat $PID` died some time ago|tee -a $TMP.mail|logger -s -puser.err -t$BASE.$$ 
-                                fi
-                           fi
-                       else
-                    echo other process `cat $PID` died some time ago|tee -a $TMP.mail|logger -s -puser.err -t$BASE.$$ 
-                fi
-            fi
-                       else
-                    echo other process `cat $PID` died some time ago|tee -a $TMP.mail|logger -s -puser.err -t$BASE.$$ 
-                fi
-            fi
-        else 
-            echo other process `cat $PID` died some time ago|tee -a $TMP.mail|logger -s -puser.err -t$BASE.$$ 
-        fi
-    fi
-    echo $$ > $PID
-}
-############################################################
-# end mutex section
-############################################################
-af6_mutex_out () {
-    rm -f $PID
-}
-############################################################
 # canonical path name
 ############################################################
 abspath () { 
@@ -337,7 +44,6 @@ abspath () {
 ############################################################
 af6_backup () {
 
-    #echo "** backup $*" | tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
     if [ -d "$1" ] ; then
         echo "Backup directory $1:"                  | tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
         find $1 -type f $LAZY -exec $MYSELF --nomail backup {} \; >> $TMP.mail
@@ -346,41 +52,52 @@ af6_backup () {
         echo "Backup directory $DIR:"                | tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
         find . -type f $LAZY -exec $MYSELF --nomail backup {} \; >> $TMP.mail
     elif [ -f "$1" ] ; then
-        af6_mutex_in
-
         MD5=`md5sum $1|cut -d' ' -f1`
+
+        MD51=`echo $MD5|cut -c1,1`
+        MD52=`echo $MD5|cut -c2,2`
+        MD53=`echo $MD5|cut -c3,3`
+
         LS=`ls -l --time-style="+%Y%m%d%H%M%S" $1`
         SIZE=`echo $LS|cut -d' ' -f5`
         MDATE=`echo $LS|cut -d' ' -f6`
         ABS=`abspath $1`
 
-        #ssh $TARGET af6Server backup $MD5 $SIZE $MDATE $HOST $ABS 
-        echo "serverBackup $MD5 $SIZE $MDATE $HOST \"$ABS\""| tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
-        #echo "DOBACKUP" > $TMP.serverOut
-        ./af6backup.sh serverBackup $MD5 $SIZE $MDATE $HOST "$ABS" | tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
+        MYPATH=$TODIR/$MD51/$MD52/$MD53/$MD5
+        LOCKDIR=$MYPATH.lock
+        COMMAND="mkdir $LOCKDIR;echo \$?"
+        RES=`ssh $TARGET $COMMAND`
+        if [ "$RES" = "0" ] ; then
+            mkdir -p $TMP 2>/dev/null
+            scp $TARGET:$MYPATH.af6 $TMP 2>/dev/null
+            echo $?
 
-        RETCODE=`tail -1 < $TMP.mail`   
-        if [ "$RETCODE" == "DOBACKUP" ] ; then
-            mkdir -p $TMP.dir
-            bzip2 --best --stdout --force $1 > $TMP.dir/$MD5.bz2
-            BZSIZE=`ls -l $TMP.dir/$MD5.bz2|cut -d' ' -f5`  
-            EINS=`echo $MD5|cut -c1-1`
-            ZWEI=`echo $MD5|cut -c2-2`
-            DREI=`echo $MD5|cut -c3-3`
-            DIR=$TODIR/$EINS/$ZWEI/$DREI
-            if [ $SIZE -gt $BZSIZE ] ; then
-                echo "BZIPPED file $ABS."|tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
-                cp $TMP.dir/$MD5.bz2 $DIR
+            echo "$MD5;$SIZE;$MDATE;$HOST;\"$ABS\"" >> $TMP/$MD5.af6
+            sort < $TMP/$MD5.af6 > $TMP/$MD5.tmp
+            uniq < $TMP/$MD5.tmp > $TMP/$MD5.af6
+            cat $TMP/$MD5.af6
+
+            NUMTARGET=`ssh $TARGET ls $MYPATH $MYPATH.bz2 2>/dev/null|wc -l`
+            if [ "$NUMTARGET" = "0" ] ; then
+                echo "DOBACKUP file $ABS"                                              |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
+                bzip2 --best --stdout --force $1 > $TMP/$MD5.bz2
+                BZSIZE=`ls -l $TMP/$MD5.bz2|cut -d' ' -f5`  
+	        
+                if [ $SIZE -gt $BZSIZE ] ; then
+                    echo "BZIPPED file $ABS."                                          |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
+                    scp $TMP/$MD5.af6 $TMP/$MD5.bz2 $TARGET:$TODIR/$MD51/$MD52/$MD53   |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
+                else
+                    echo "COPIED  file $ABS."                                          |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
+                    scp $TMP/$MD5.af6  $TARGET:$TODIR/$MD51/$MD52/$MD53                |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
+                    scp $1 $TARGET:$MYPATH
+                fi
             else
-                echo "COPIED  file $ABS."|tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
-                cp $1 $DIR/$MD5
+                echo "ALREADYDONE file $ABS"                                           |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
+                scp $TMP/$MD5.af6 $TARGET:$TODIR/$MD51/$MD52/$MD53                     |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
             fi
-        elif [ "$RETCODE" == "ALREADYDONE" ] ; then
-            echo "This file $ABS was already backed up."|tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
-        else
-            echo "Strange retcode $RETCODE"|tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
+
+            ssh $TARGET "rmdir $LOCKDIR"
         fi
-        af6_mutex_out 
     fi
     af6_end 0
 }
@@ -499,86 +216,6 @@ af6_fromcron () {
     af6_end 0 
 }
 ############################################################
-# do the backup (server side)
-############################################################
-af6_serverBackup () {
-
-    if [ ! -d "$TODIR/f/f/f" ] ; then
-        mkdir -p $TODIR  2>/dev/null
-        for I in 0 1 2 3 4 5 6 7 8 9 a b c d e f
-          do
-            for J in 0 1 2 3 4 5 6 7 8 9 a b c d e f
-              do
-                for K in 0 1 2 3 4 5 6 7 8 9 a b c d e f
-                  do
-                    mkdir -p $TODIR/$I/$J/$K 2>/dev/null
-                    chmod 6755 $TODIR/$I/$J/$K 
-                  done
-              done
-          done
-    fi
-
-    # todo check params    
-    MD5=$1
-    #echo "MD5=$MD5"|logger -s -puser.info -t$BASE.$$
-    SIZE=$2
-    #echo "SIZE=$SIZE"|logger -s -puser.info -t$BASE.$$
-    MDATE=$3
-    #echo "MDATE=$MDATE"|logger -s -puser.info -t$BASE.$$
-    HOST=$4
-    #echo "HOST=$HOST"|logger -s -puser.info -t$BASE.$$
-    ABS=$5
-    #echo "ABS=$ABS"|logger -s -puser.info -t$BASE.$$
-
-    PATTERN=`echo $MD5|cut -c1-3`
-    af6_legacyCombine $PATTERN
-
-    echo "$MD5;$SIZE;$MDATE;$HOST;$ABS" | awk -f $TMP.serverBackup.awk -F';' \
-        -v pattern=$PATTERN -v inlist=$NAMESDIR/$PATTERN -v outlist=$NAMESDIR/$PATTERN.$$ 
-    
-    diff $NAMESDIR/$PATTERN $NAMESDIR/$PATTERN.$$ | grep '^< ' | cut -c3- > $OLDDIR/$PATTERN.$DATE
-
-    bzip2 --best --force $NAMESDIR/$PATTERN
-    mv $NAMESDIR/$PATTERN.$$ $NAMESDIR/$PATTERN 
-}
-############################################################
-# merge legacy name list
-############################################################
-af6_legacyCombine () {
-
-    mkdir -p $NAMESDIR  2>/dev/null
-    mkdir -p $OLDDIR  2>/dev/null
-
-    if [ -z "$1" ] ; then 
-        for I in 0 1 2 3 4 5 6 7 8 9 a b c d e f
-          do
-            for J in 0 1 2 3 4 5 6 7 8 9 a b c d e f
-              do
-                for K in 0 1 2 3 4 5 6 7 8 9 a b c d e f
-                  do
-                    $MYSELF combine $I$J$K
-                  done
-              done
-          done
-    else 
-        #af6_mutex_in
-        PATTERN=`echo $1|cut -c1-3|grep '^[0-9a-f][0-9a-f][0-9a-f]$'`
-        if [ -n "$PATTERN" ] ; then 
-            if [ -s $LEGACYLIST ] ; then
-                if [ $LEGACYLIST -nt $NAMESDIR/$PATTERN ] ; then
-                    awk -f $TMP.legacyCombine.awk -F';' \
-                        -v pattern=$PATTERN -v inlist=$NAMESDIR/$PATTERN \
-                        -v outlist=$NAMESDIR/$PATTERN.$$ < $LEGACYLIST
-                    diff $NAMESDIR/$PATTERN $NAMESDIR/$PATTERN.$$ | grep '^< ' | cut -c3- > $OLDDIR/$PATTERN.$DATE
-    
-                    bzip2 --best --force $NAMESDIR/$PATTERN
-                    mv $NAMESDIR/$PATTERN.$$ $NAMESDIR/$PATTERN 
-                fi
-            fi
-        fi
-    fi
-}
-############################################################
 # main
 ############################################################
 if [ "$1" = "--force" ] ; then
@@ -614,10 +251,6 @@ if [ "$1" = "backup" ] ; then
     af6_backup $2
 elif [ "$1" = "fromcron" ] ; then
     af6_fromcron
-elif [ "$1" = "serverBackup" ] ; then
-    #af6_mutex_in
-    shift
-    af6_serverBackup $*
 else
     cat<<EOF
 
