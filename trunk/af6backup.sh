8eb9274e8b42d0a281fdf3675360b6a25eb409af
alexander.franz.1411@gmail.com <alexander.franz.1411@gmail.com@884ed0b3-71b4-c2c8-87cb-37b2ad9281ac>
2014-05-23 22:04:00 +0000

    geht

--- trunk/af6backup.sh
+++ trunk/af6backup.sh
@@ -71,30 +71,28 @@ af6_backup () {
         if [ "$RES" = "0" ] ; then
             mkdir -p $TMP 2>/dev/null
             scp $TARGET:$MYPATH.af6 $TMP 2>/dev/null
-            echo $?
 
             echo "$MD5;$SIZE;$MDATE;$HOST;\"$ABS\"" >> $TMP/$MD5.af6
             sort < $TMP/$MD5.af6 > $TMP/$MD5.tmp
             uniq < $TMP/$MD5.tmp > $TMP/$MD5.af6
-            cat $TMP/$MD5.af6
 
             NUMTARGET=`ssh $TARGET ls $MYPATH $MYPATH.bz2 2>/dev/null|wc -l`
             if [ "$NUMTARGET" = "0" ] ; then
-                echo "DOBACKUP file $ABS"                                              |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
+                echo "DOBACKUP file $ABS"                          |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
                 bzip2 --best --stdout --force $1 > $TMP/$MD5.bz2
                 BZSIZE=`ls -l $TMP/$MD5.bz2|cut -d' ' -f5`  
 	        
                 if [ $SIZE -gt $BZSIZE ] ; then
-                    echo "BZIPPED file $ABS."                                          |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
-                    scp $TMP/$MD5.af6 $TMP/$MD5.bz2 $TARGET:$TODIR/$MD51/$MD52/$MD53   |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
+                    echo "BZIPPED file $ABS. ($SIZE > $BZSIZE)"    |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
+                    scp $TMP/$MD5.af6 $TMP/$MD5.bz2 $TARGET:$TODIR/$MD51/$MD52/$MD53 
                 else
-                    echo "COPIED  file $ABS."                                          |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
-                    scp $TMP/$MD5.af6  $TARGET:$TODIR/$MD51/$MD52/$MD53                |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
-                    scp $1 $TARGET:$MYPATH
+                    echo "COPIED  file $ABS. ($SIZE)"              |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
+                    scp $TMP/$MD5.af6  $TARGET:$TODIR/$MD51/$MD52/$MD53  
+                    scp $1 $TARGET:$MYPATH 2>&1
                 fi
             else
-                echo "ALREADYDONE file $ABS"                                           |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
-                scp $TMP/$MD5.af6 $TARGET:$TODIR/$MD51/$MD52/$MD53                     |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
+                echo "ALREADYDONE file $ABS"                       |tee -a $TMP.mail |logger -s -puser.info -t$BASE.$$
+                scp $TMP/$MD5.af6 $TARGET:$TODIR/$MD51/$MD52/$MD53    
             fi
 
             ssh $TARGET "rmdir $LOCKDIR"
@@ -105,13 +103,15 @@ af6_backup () {
 ############################################################
 af6_end () {
     if [ ! "$NOMAIL" = "true" ] ; then
-        echo '-------------------------------------------------------------'|tee -a $TMP.mail|logger -s -puser.info -t$BASE.$$
+        echo '-------------------------------------------------------------'|\
+             tee -a $TMP.mail|logger -s -puser.info -t$BASE.$$
         STOP=`date +%s`
         DIFF=`expr $STOP - $START`
         DIFFH=`expr $DIFF / 3600`
         DIFFM=`expr \( $DIFF - \( $DIFFH \* 3600 \) \) / 60`
         DIFFS=`expr $DIFF % 60`
-        echo|awk "{printf(\"It took me %d:%02d:%02d to get here with RC %d\n\",$DIFFH,$DIFFM,$DIFFS,$1)}"|tee -a $TMP.mail|logger -s -puser.info -t$BASE.$$
+        echo|awk "{printf(\"It took me %d:%02d:%02d to get here with RC %d\n\",$DIFFH,$DIFFM,$DIFFS,$1)}"|\
+             tee -a $TMP.mail|logger -s -puser.info -t$BASE.$$
 
         NUMDO=`grep DOBACKUP < $TMP.mail|wc -l`
         NUMDONE=`grep ALREADYDONE < $TMP.mail|wc -l`
@@ -120,6 +120,8 @@ af6_end () {
         NUMBZ=`grep BZIPPED < $TMP.mail|wc -l`
         NUMCP=`grep COPIED  < $TMP.mail|wc -l`
         echo|awk "{printf(\"%d files bzipped, %d files copied\n\",$NUMBZ,$NUMCP)}"|tee -a $TMP.mail|logger -s -puser.info -t$BASE.$$
+
+        ssh $TARGET df | grep _DATA |tee -a $TMP.mail|logger -s -puser.info -t$BASE.$$
 
         if [ $1 -ne 99 ] ; then
             #
@@ -139,79 +141,52 @@ af6_end () {
     fi
 }
 #############################################################
-# 
+# frame if called from cron
 #############################################################
 af6_fromcron () {
 
     # only once daily
     NOW=`date +%s`
-    if [ -f $TODIR/$BASE.stamp ] ; then
-        LAST=`cat $TODIR/$BASE.stamp`
+    if [ -f $TMP.stamp ] ; then
+        LAST=`cat $TMP.stamp`
         DIFF=`expr $NOW - $LAST`
         # slightly drifting backwards
         if [ "$DIFF" -lt "64800" ] ; then
             echo Not necessary yet.|logger -s -puser.info -t$BASE.$$
             af6_end 99
         else 
-            echo $NOW > $TODIR/$BASE.stamp
+            echo $NOW > $TMP.stamp
         fi
     else 
-        echo $NOW > $TODIR/$BASE.stamp
+        echo $NOW > $TMP.stamp
     fi
 
     export FROMCRON=1
-    af6_clean
     if [ "$HOST" = "elefant" ] ; then
-        af6_combine
 
-        #DOM=`date +%d`
-        #if [ "$DOM" = "13" ] ; then
-        #    # once a month checking
-        #    af6_check
-        #else
-        #    if [ "$DOM" = "26" ] ; then
-        #        # once a month not lazy
-        #        export LAZY=""
-        #    fi
-            af6_backup /3data/Fotos
-            af6_backup /1data/Video
-            af6_backup /1data/astrid
-            af6_backup /1data/ich
-            af6_backup /0data/Musik
-            af6_backup /root
-            af6_backup /etc
-            af6_backup /1data/physhome/astrid
-            af6_backup /1data/physhome/astrid.old
-            af6_backup /1data/physhome/ich.old
-            af6_backup /home/ich
-            af6_backup /data/repository/repos
-	    
-            af6_backup /lost+found
-            af6_backup /0data/lost+found
-            af6_backup /1data/lost+found
-            af6_backup /2data/lost+found
-            af6_backup /3data/lost+found
+        af6_backup /3data/Fotos
+        af6_backup /1data/Video
+        af6_backup /1data/astrid
+        af6_backup /1data/ich
+        af6_backup /0data/Musik
+        af6_backup /root
+        af6_backup /etc
+        af6_backup /1data/physhome/astrid
+        af6_backup /1data/physhome/astrid.old
+        af6_backup /1data/physhome/ich.old
+        af6_backup /home/ich
+        af6_backup /data/repository/repos
+	
+        af6_backup /lost+found
+        af6_backup /0data/lost+found
+        af6_backup /1data/lost+found
+        af6_backup /2data/lost+found
+        af6_backup /3data/lost+found
             
-            /sbin/mount.cifs //drache/C /dracheC -o user=ich,password=cc440a08 2>&1  > $TMP.mountout
-            RC=$?
-            echo "RC $RC" >>  $TMP.mountout
-            cat $TMP.mountout >> $TMP.mail
-            logger -s -puser.info -t$BASE.$$ < $TMP.mountout
-            if [ "$RC" = "0" ] ; then
-                 af6_backup /dracheC/Users/ich/Documents
-                 af6_backup /dracheC/Users/ich/Videos
-                 af6_backup /dracheC/Users/ich/Music
-                 af6_backup /dracheC/Users/ich/AppData
-                 sleep 3
-                 /bin/umount /dracheC
-            fi
-                
-            af6_combine
-        #fi 
+    else
 
-        df $MOUNTDIR |tee -a $TMP.mail|logger -s -puser.info -t$BASE.$$
-        df $MOUNTDIR2|tee -a $TMP.mail|logger -s -puser.info -t$BASE.$$
-        #af6_finally
+        af6_backup /home/ich
+
     fi
     FROMCRON=0
     af6_end 0 
