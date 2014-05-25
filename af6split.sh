f26f6236099d88aee34d249e36fc9ab2a68d6c45
alexander.franz.1411@gmail.com <alexander.franz.1411@gmail.com@884ed0b3-71b4-c2c8-87cb-37b2ad9281ac>
2014-05-25 07:27:03 +0000

    geht

--- af6split.sh
+++ af6split.sh
@@ -26,6 +26,29 @@ MOUNTDIR=/share/HDA_DATA/Public
 TODIR=$MOUNTDIR/af5backup.dir
 OLDDIR=$TODIR/old
 LEGACYLIST=$TODIR/af5backup.names
+############################################################
+# canonical path name
+############################################################
+
+############################################################
+# canonical path name
+############################################################
+mdate () {
+    if [ -f $1 ] ; then 
+        _LS=`ls -l --time-style="+%Y%m%d%H%M%S" $1 2>/dev/null`
+        _MDATE=`echo $_LS|cut -d' ' -f6`
+        if [ -z "$_MDATE" ] ; then
+            _LS=`ls -le $1 2>/dev/null`
+            _Y=`echo $_LS|cut -d' ' -f10`
+            _MO=`echo $_LS|cut -d' ' -f7|sed -e's+Jan+01+' -e's+Feb+02+' -e's+Mar+03+' -e's+Apr+04+' -e's+Ma.+05+' -e's+Jun+06+' -e's+Jul+07+' -e's+Aug+08+' -e's+Sep+09+' -e's+O.t+10+' -e's+Nov+11+' -e's+De.+12+'`
+            _D=`echo $_LS|cut -d' ' -f8|awk '{printf("%02d",\$0)}'`
+            _T=`echo $_LS|cut -d' ' -f9|sed -e's+:++g'`
+            _MDATE=`echo $_Y$_MO$_D$_T`
+        fi 
+        echo $_MDATE
+    fi
+}
+
 #############################################################
 if [ ! -d "$TODIR/f/f/f" ] ; then
     mkdir -p $TODIR  2>/dev/null
@@ -42,7 +65,7 @@ if [ ! -d "$TODIR/f/f/f" ] ; then
       done
 fi
 
-wc -l $LEGACYLIST 
+#wc -l $LEGACYLIST 
 
 while read LINE
   do
@@ -56,18 +79,17 @@ while read LINE
     rm $MYPATH.check 2>/dev/null
     rm $MYPATH.bz2.check 2>/dev/null
 
-    LS=`ls -l --time-style="+%Y%m%d%H%M%S" $MYPATH 2>/dev/null`
-    if [ -z "$LS" ] ; then
-        LS=`ls -l --time-style="+%Y%m%d%H%M%S" $MYPATH.bz2 2>/dev/null`
+    MDATE=`mdate $MYPATH`
+    if [ -z "$MDATE" ] ; then
+        MDATE=`mdate $MYPATH.bz2`
     fi 
-    MDATE=`echo $LS|cut -d' ' -f6`
     if [ -z "$MDATE" ] ; then
         MDATE=20131111111111
     fi 
 
     SIZE=`echo $LINE|cut -f2 -d';'`
     ABS=`echo $LINE|sed -e's+[^;]*;[^;]*;[^;]*;++'`
-
+    
     echo "$MD5;$SIZE;$MDATE;$HOST;\"$ABS\"" >> $MYPATH.af6
     sort < $MYPATH.af6 > $TMP.af6
     uniq < $TMP.af6 > $MYPATH.af6
